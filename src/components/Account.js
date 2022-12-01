44c25dd0e3d92904ea6b296fa248693e346e6a27
Mohammad Shafi <shafi.bahrami.2015@gmail.com>
2022-12-01 11:47:17 +0100

    Dashboard added

--- src/components/Account.js
+++ src/components/Account.js
@@ -25,19 +25,26 @@ const Account=()=>{
 
    return (
       <div>
-         <p>Account: {user.email}</p>
-         <button onClick={logOutFromSession}>Log out</button>
-         <div>
-            <div>
+         <div className='flex justify-between'>
+            <p className='ml-6'>Account: {user.email}</p>
+            <button className='mr-6' onClick={logOutFromSession}>Log out</button>
+         </div>
+         <h1>Your dashboard</h1>
+         <div className='flex flex-row flex-wrap p-4 w-3/5 justify-between mx-auto mt-4'>
+            <div className='border border-solid border-2 w-96 m-2 mb-8 ml-8 ease-in duration-300 hover:scale-110 cursor-pointer'>
+               <img src="https://www.designyourway.net/blog/wp-content/uploads/2012/08/winterph2.jpg" alt="cool"/>
                <Link to="/account/post-help">Post Help</Link>
             </div>
-            <div>
+            <div className='border border-solid border-2 w-96 m-2 mb-8 mr-8 ease-in duration-300 hover:scale-110 cursor-pointer'>
+               <img src="https://www.designyourway.net/blog/wp-content/uploads/2012/08/winterph2.jpg" alt="cool"/>
                <Link to="/account/search-help">Search Help</Link>
             </div>
-            <div>
+            <div className='border border-solid border-2 w-96 m-2 mb-8 ml-8 ease-in duration-300 hover:scale-110 cursor-pointer'>
+               <img src="https://www.designyourway.net/blog/wp-content/uploads/2012/08/winterph2.jpg" alt="cool"/>
                <Link to="/account/my_posts">My Posts</Link>
             </div>
-            <div>
+            <div className='border border-solid border-2 w-96 m-2 mb-8 mr-8 ease-in duration-300 hover:scale-110 cursor-pointer'>
+               <img src="https://www.designyourway.net/blog/wp-content/uploads/2012/08/winterph2.jpg" alt="cool"/>
                <Link to="/account/applied">Applied Jobs</Link>
             </div>
          </div>
