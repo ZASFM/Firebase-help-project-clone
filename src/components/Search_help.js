54863e7e63266771ec5af0c71ebf7abe37375736
Mohammad Shafi <shafi.bahrami.2015@gmail.com>
2022-12-02 10:58:21 +0100

    Search inpu && styling

--- src/components/Search_help.js
+++ src/components/Search_help.js
@@ -1,17 +1,41 @@
-import React from "react";
+import React,{useRef,useEffect,useState} from "react";
 import { UserAuth } from "../AuthContext/context";
 
 const SearchHelp=()=>{
    const {databaseData}=UserAuth();
-   if(databaseData!==[]){
-      console.log(databaseData);
+   const [localData,setLocalData]=useState([]);
+   const input=useRef();
+
+   useEffect(()=>{
+      setLocalData(databaseData);
+      input.current.focus();
+   },[databaseData])
+
+   const filterItems=()=>{
+      let filterItems=databaseData.filter(data=>data.name.includes(input.current.value));
+      setLocalData(filterItems);
    }
+
    return (
       <div>
-         {databaseData.map(data=>{
+         <input 
+            type="text" 
+            placeholder="Search by name of the user"
+            ref={input}
+            onChange={filterItems}
+            className="w-3/5 border-2 rounded p-2 "
+         />
+         <h1 className="text-2xl">All posts are show here:</h1>
+         {localData.length<1 && <div>No results matches your search, try with another query</div>}
+         {localData!==[] && localData.map(data=>{
             return (
-               <div className="border border-solid border-2">
-                  {data.name}
+               <div className="border border-solid border-2 border-black bg-blue-300 m-4">
+                  <div className="w-full flex justify-center items-center">
+                     <div className="bg-black-300 border-2 border-solid rounded w-1/5 flex justify-center items-center m-4">{data.location}</div>
+                     <div className="bg-black-300 border-2 border-solid rounded w-1/5 flex justify-center items-center m-4">{data.category}</div>
+                  </div><br/>
+                  <div className="pl-4">{`From: ${data.name}`}</div>
+                  <div className="pl-4 mb-2">{`Message: ${data.message}`}</div>
                </div>
             )
          })}
